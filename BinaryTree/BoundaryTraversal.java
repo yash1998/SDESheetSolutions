7f83ecb30325ffdf63841e39723039c2f6375243
Yashwant Vadali <yashwant.yash.vadali@gmail.com>
2023-08-30 15:08:48 +0530

    fixes

--- BinaryTree/BoundaryTraversal.java
+++ BinaryTree/BoundaryTraversal.java
@@ -9,68 +9,69 @@ public class BoundaryTraversal {
     }
 
     public static void left(TreeNode curr, List<Integer> result) {
-        while(curr != null && !isLeafNode(curr)){
+        while (curr != null && !isLeafNode(curr)) {
             result.add(curr.data);
             if (curr.left != null) {
                 curr = curr.left;
-            } else if (curr.right != null) {
+            } else {
                 curr = curr.right;
             }
         }
     }
 
     public static void right(TreeNode curr, List<Integer> right) {
-        while(curr != null && !isLeafNode(curr)) {
+        while (curr != null && !isLeafNode(curr)) {
             right.add(0, curr.data);
             if (curr.right != null) {
                 curr = curr.right;
-            } else if (curr.left != null) {
+            } else {
                 curr = curr.left;
             }
         }
     }
-    
-    public static List<Integer> traverseBoundary(TreeNode root){
-        // Write your code here.
+
+    public static List<Integer> traverseBoundary(TreeNode root) {
         List<Integer> result = new ArrayList<>();
 
-        if(root == null) return result;
+        if (root == null)
+            return result;
 
         result.add(root.data);
-        
+
         left(root.left, result);
 
         inorder(root, result);
-        
+
         List<Integer> right = new ArrayList<>();
         right(root.right, right);
-        
+
         result.addAll(right);
-        
+
         return result;
     }
 
     public static void inorder(TreeNode root, List<Integer> res) {
-        if (root==null) return;
+        if (root == null)
+            return;
         if (isLeafNode(root)) {
             res.add(root.data);
             return;
         }
-        
+
         inorder(root.left, res);
         inorder(root.right, res);
     }
 
     class TreeNode {
-     int data;
-     TreeNode left;
-     TreeNode right;
+        int data;
+        TreeNode left;
+        TreeNode right;
 
-     TreeNode(int data) {
-         this.data = data;
-         this.left = null;
-         this.right = null;
-     }
- }
-    
+        TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
 }
